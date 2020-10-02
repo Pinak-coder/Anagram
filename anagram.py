dec6f5b58b67212c283b3d48ee8062a1f8396a51
Pinak Datta <pinakdatta2002@gmail.com>
2020-10-02 12:07:50 +0530

    A much Simpler way to check for Anagrams

--- anagram.py
+++ anagram.py
@@ -1,32 +1,13 @@
-
-def anagram(string_a,string_b):
-  dict_a = {}
-  dict_b = {}
-  int_i = 0
-  int_j = 0
-  while int_i < len(string_a):
-    if string_a[int_i] in dict_a:
-      dict_a[string_a[int_i]] += 1
-    else:
-      dict_a[string_a[int_i]] = 1
-    int_i +=1
-  print(dict_a)
-
-  while int_j < len(string_b):
-    if string_b[int_j] in dict_b:
-      dict_b[string_b[int_j]] += 1
-    else:
-      dict_b[string_b[int_j]] = 1
-    int_j +=1
-  print(dict_b)  
-
-  int_k = 0
-  while int_k < len(string_a):
-    if string_a[int_k] in dict_b and dict_a[string_a[int_k]] == dict_b[string_a[int_k]]:
-      int_k +=1
-    else:
-      return "it isnt"
-  return "its an anagram lol"
-
-print(anagram("hello","helol"))
-    
+from collections import Counter 
+def is_anagram(str1, str2): 
+     return Counter(str1) == Counter(str2) 
+  
+# or without having to import anything  
+def is_anagram(str1, str2):  
+    return sorted(str1) == sorted(str2)  
+s1=input('Enter first String: ')
+s2=input('Enter 2nd String: ')
+if (is_anagram(s1,s2)):
+    print('They are Anagrams')
+else:
+    print('They are not Anagrams')
